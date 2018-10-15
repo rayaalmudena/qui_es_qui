97a4e0a42fcc5e1745200c4743e685503d5e325d
rayaalmudena <raya.almudena@gmail.com>
2018-10-15 19:54:18 +0200

    random a adivinar

--- index.php
+++ index.php
@@ -8,11 +8,18 @@
 	<?php 
 	function cartaElegida(){
 	$cartaDir = 'cartas/*';
-	$cartas = glob($cartaDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+	$cartas = glob($cartaDir.'*.png', GLOB_BRACE);
 	$cartaRandom = $cartas[array_rand($cartas)];
-	echo "<img src='$cartaRandom'>\n";
+	echo "<img height='200' src='$cartaRandom'>\n";
 	}
+	
+	//function cartasRandom(){
+	$cartaDir = 'cartas/*';
+	$cartas = glob($cartaDir.'*.png', GLOB_BRACE);
+	
+	//}	
 	cartaElegida();
+	//cartaRandom();
 	 ?>
 
 </body>
