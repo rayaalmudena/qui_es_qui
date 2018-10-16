998da8e9e8add3148d87991f3a72463c0968ddfa
rayaalmudena <raya.almudena@gmail.com>
2018-10-16 18:04:35 +0200

    random a adivinar

--- index.php
+++ index.php
@@ -6,13 +6,34 @@
 <body>
 
 	<?php 
-	function cartaElegida(){
-		$cartaDir = 'cartas/*';
-		$cartas = glob($cartaDir.'*.png');
-		$cartaRandom = $cartas[array_rand($cartas)];
-		echo "<img height='200' src='$cartaRandom'>\n";
+
+	function arrayCartas(){
+		$cartas=[];
+		$contador=1;
+		$file = fopen("imagenes.txt", "r");
+			while($contador<=12) {
+				$cartaNueva;
+				$carta=fgets($file);
+				$carta=trim($carta);
+				$carta=explode('_',$carta);
+				$nombre="$carta[0]";
+				$gafas="$carta[2]";
+				$cabello="$carta[4]";
+				$sexo="$carta[6]";
+				$contador=$contador+1;
+				$cartaNueva= array(0=>$nombre,1=>$gafas,2=>$cabello,3=>$sexo);
+				$cartas[]=$cartaNueva;			
+			}
+			fclose($file);
+			return $cartas;
 	}
-	cartaElegida();	
+	function cartaElegida($cartas){
+		$cartaElegida = $cartas[array_rand($cartas)];
+		echo "<img height='200' src='cartas/$cartaElegida[0]' class='cartaElegida cartas' gafas='$cartaElegida[1]' cabello='$cartaElegida[2]' sexo='$cartaElegida[3]'>";
+	}
+
+	
+	cartaElegida(arrayCartas());
 	
 	
 	 ?>
