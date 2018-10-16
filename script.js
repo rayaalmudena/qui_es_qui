cbe3bc345e67c6f3e78dafdb3015f9e6ca636801
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-10-16 18:06:10 +0200

    flip function

---
+++ script.js
@@ -0,0 +1,16 @@
+var cardNames = [
+	"mago",
+	"dragon",
+	"exodia"
+];
+
+function flip(num) {
+	var carta = cardNames[num];
+	
+	var elementos = document.getElementsByName(carta)[0];
+
+	elementos.setAttribute(carta, "back");
+	elementos.removeAttribute(carta);
+	
+	document.back.src="back.png";
+}
