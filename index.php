9c11cd413d66f7ea44298908c326761bf60953a3
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-10-16 18:06:10 +0200

    flip function

--- index.php
+++ index.php
@@ -2,19 +2,31 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">	
+	<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
 
 	<?php 
-	$imagesDir = 'cartas/*';
+	/*$imagesDir = 'cartas/*';
+*
+*	$images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+*
+*	$randomImage = $images[array_rand($images)];
+*	echo "<img src='$randomImage'>\n";
+*
+*
+*/	 ?>
 
-	$images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+	
+	<div class="flip">
+		<img src="magooscuro.jpg" class="card" name="mago" onclick="flip(0)"/>
+		<img src="blueeyes.jpg" class="card" name="back" onclick="flip(1)"/>
+		<img src="exodia.jpg" class="card" name="exodia" onclick="flip(2)"/>
+	</div>
 
-	$randomImage = $images[array_rand($images)];
-	echo "<img src='$randomImage'>\n";
 
 
-	 ?>
 
 </body>
 </html>
