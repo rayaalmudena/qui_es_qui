d8a6a1401f83f1416b76b231a81060ec29f99859
rayaalmudena <raya.almudena@gmail.com>
2018-10-17 13:28:43 +0200

    tablero

--- index.php
+++ index.php
@@ -13,35 +13,67 @@
 		$cartas=[];
 		$contador=1;
 		$file = fopen("imagenes.txt", "r");
-			while($contador<=12) {
-				$cartaNueva;
-				$carta=fgets($file);
-				$carta=trim($carta);
-				$carta=explode('_',$carta);
-				$nombre="$carta[0]";
-				$gafas="$carta[2]";
-				$cabello="$carta[4]";
-				$sexo="$carta[6]";
-				$contador=$contador+1;
-				$cartaNueva= array(0=>$nombre,1=>$gafas,2=>$cabello,3=>$sexo);
-				$cartas[]=$cartaNueva;			
-			}
-			fclose($file);
-			return $cartas;
+		while (!feof($file)) {
+			$carta=urlencode(trim(fgets($file)));
+			if (empty($carta)) {
+				# salto
+			} else {
+				array_push($cartas,$carta);
+			} 	
+  		}
+    	fclose($file);
+    	shuffle($cartas);
+
+    	foreach ($cartas as $carta) {
+    		$cartaNueva;
+			$carta=explode('_',$carta);
+			$nombre="$carta[0]";
+			$gafas="$carta[2]";
+			$cabello="$carta[4]";
+			$sexo="$carta[6]";
+			$contador=$contador+1;
+			$cartaNueva= array("nombre"=>$nombre,"gafas"=>$gafas,"cabello"=>$cabello,"sexo"=>$sexo);
+			$cartasFinal[]=$cartaNueva;	
+    	}
+		return $cartasFinal;
 	}
 	function cartaElegida($cartas){
-		$cartaElegida = $cartas[array_rand($cartas)];
-		echo "<img height='200' src='cartas/$cartaElegida[0]' class='cartaElegida cartas' gafas='$cartaElegida[1]' cabello='$cartaElegida[2]' sexo='$cartaElegida[3]'>";
+		$cartaElegida = $cartas[0];
+		echo "<img height='200' src='cartas/$cartaElegida[nombre]' class='cartaElegida' gafas='$cartaElegida[gafas]' cabello='$cartaElegida[cabello]' sexo='$cartaElegida[sexo]'>";
 	}
-	<div class="flip">
+
+	function tableroCartas($cartas){
+		$tabla='<table cellpadding="5" cellspacing="10" bgcolor="green"><tr>';
+		$c = 0;
+
+		while ($c< count($cartas)){
+			$carta=$cartas[$c];
+			$tabla .="\n\t<td style='border:3px solid grey'><img height='160' src='cartas/$carta[nombre]' class='carta$c carta' gafas='$carta[gafas]' cabello='$carta[cabello]' sexo='$carta[sexo] name='$carta[nombre]''><td>";
+			$c++;
+			if ($c==count($cartas)){
+				$tabla .="</tr>\n";	
+				$tabla .="</table>";
+			}
+			if ($c%6==0 && $c!=count($cartas)){
+				$tabla .="\n</tr><tr>";
+			}
+		}
+		
+		return $tabla;
+	}
+	
+
+	/*<div class="flip">
 		<img src="magooscuro.jpg" class="card" name="mago" onclick="flip(0)"/>
 		<img src="blueeyes.jpg" class="card" name="back" onclick="flip(1)"/>
 		<img src="exodia.jpg" class="card" name="exodia" onclick="flip(2)"/>
-	</div>
+	</div>**/
 
-	
 	cartaElegida(arrayCartas());
-	 ?>
+	echo tableroCartas(arrayCartas());
+
+	?>
+
 
 </body>
 </html>
