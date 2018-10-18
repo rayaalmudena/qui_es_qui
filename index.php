31cad6ac1ed50889b11d01f51925302cb905a810
rayaalmudena <raya.almudena@gmail.com>
2018-10-18 20:07:04 +0200

    sin _ en txt document

--- index.php
+++ index.php
@@ -9,27 +9,20 @@
 <body>
 	
 	<?php 
-
 	//Comienzo de los posibles errores
 	//Los errores apareceran por orden que indica en la guia del leandro, primero apareceran los nombres repetidos, luego los atributos repetidos y luego los atributos que no existan en el config
-
 	function errores(){
 				//Archivo de configuracion, para el Punto 3 errores
 			$config_array=[];
 			$config_array_gafas=[];
 			$config_array_cabello=[];
 			$config_array_sexo=[];
-
 			$file2 = fopen("config.txt", "r");
 			$z=0;
-
 			while(!feof($file2)) {
-
 				$config_array[$z]=fgets($file2). "_<br />";
-
 				$z=$z+1;
 			}
-
 				//guarda en un array el archivo de configuracion, es el array de las gafas
 				if (strpos($config_array[0], '_')){//si no pongo esto, la ultima linea no me la compara
 					list($config_gafas_1, $config_gafas_2, $config_gafas_3) = explode("_", $config_array[0]);
@@ -37,7 +30,6 @@
 					$config_array_gafas[1]=$config_gafas_2;
 					$config_array_gafas[2]=substr($config_gafas_3, 0, -1);//siempre tiene saltos de linea
 				}
-
 				//guarda en un array el archivo de configuracion, es el array del cabello
 				if (strpos($config_array[1], '_')){//si no pongo esto, la ultima linea no me la compara
 					list($config_cabello_1, $config_cabello_2, $config_cabello_3, $config_cabello_4) = explode("_", $config_array[1]);
@@ -46,7 +38,6 @@
 					$config_array_cabello[2]=$config_cabello_3;
 					$config_array_cabello[3]=substr($config_cabello_4, 0, -1);//siempre tiene saltos de linea
 				}
-
 				//guarda en un array el archivo de configuracion, es el array del sexo
 				if (strpos($config_array[2], '_')){//si no pongo esto, la ultima linea no me la compara
 					list($config_sexo_1, $config_sexo_2, $config_sexo_3) = explode("_", $config_array[2]);
@@ -55,7 +46,6 @@
 					$config_array_sexo[2]=$config_sexo_3;
 				}
 			fclose($file2);
-
 			//Punto 2 errores
 			$array=[];
 			$i=0;
@@ -63,13 +53,9 @@
 			$respuesta_gafas=[];
 			$respuesta_cabello=[];
 			$respuesta_sexo=[];
-
 			$file = fopen("imagenes.txt", "r");
-
 			while(!feof($file)) {
-
 				$array[$i]=fgets($file);
-
 				if (strpos($array[$i], '_')){//si no pongo esto, la ultima linea no me la compara
 					list($nombre, $gafas, $gafas2, $cabello, $cabello2, $sexo, $sexo2) = explode("_", $array[$i]);
 					$nombre_foto[$i]=$nombre;
@@ -80,8 +66,6 @@
 				$i=$i+1;
 			}
 			fclose($file);
-
-
 			//con esto de aqui, detecta si hay un nombre repetido, y lo indica (punto 1 de errores)
 			$repeated = array_filter(array_count_values($nombre_foto), function($count) {
 			    return $count > 1;
@@ -90,14 +74,8 @@
 			    echo "La imagen '$key' se repite $value veces <br />";
 			    return false;
 			}
-
-
-
-
 			for ($i=0; $i < count($nombre_foto); $i++) { 
-
 				for ($x=$i+1; $x < count($nombre_foto); $x++) { 
-
 					if ($respuesta_gafas[$i]==$respuesta_gafas[$x] && $respuesta_cabello[$i]==$respuesta_cabello[$x] && $respuesta_sexo[$i]==$respuesta_sexo[$x]){
 							
 						echo "$i, $x <br>";
@@ -111,14 +89,12 @@
 						echo ", y su sexo es: ";
 						echo $respuesta_sexo[$x];
 						echo "<br>";
-
 						echo(" esta REPETIDO <br> <br>");
 						return false;
 					}
 				}
 			}
 			//punto 3 de errores otra vez
-
 			foreach ($nombre_foto as $key => $value) {
 				if (in_array($respuesta_gafas[$key], $config_array_gafas)) {
 			    	/*echo "existe gafas: $key, de $value <br>";*/
@@ -145,21 +121,11 @@
 			return true;
 		}
 //fin de los errores
-
-
-
-
-
-
-
-
 		if (errores()!=true) {//En caso de que salga un error, no se iniciara el programa, y saldra el error
 			echo "No se puede iniciar el programa <br>";
 		}
 		else{//en caso de que no hayan errores, se iniciara el programa:
 		
-
-
 			?>
 
 
@@ -208,7 +174,9 @@
 				$contador=1;
 				$file = fopen("imagenes.txt", "r");
 				while (!feof($file)) {
-					$carta=trim(fgets($file));
+					$carta =fgets($file);
+					$carta=str_replace(":", "", $carta);
+					$carta=rtrim($carta, "\n");
 					if (empty($carta)) {
 						# salto
 					} else {
@@ -217,17 +185,16 @@
 		  		}
 		    		fclose($file);
 		    		shuffle($cartas);
-
 		    		foreach ($cartas as $carta) {
-					$cartaNueva;
-					$carta=explode('_',$carta);
-					$nombre="$carta[0]";
-					$gafas="$carta[2]";
-					$cabello="$carta[4]";
-					$sexo="$carta[6]";
-					$contador=$contador+1;
-					$cartaNueva= array("nombre"=>$nombre,"gafas"=>$gafas,"cabello"=>$cabello,"sexo"=>$sexo);
-					$cartasFinal[]=$cartaNueva;	
+						$cartaNueva;
+						$carta=explode(' ',$carta);
+						$nombre="$carta[0]";
+						$gafas="$carta[2]";
+						$cabello="$carta[4]";
+						$sexo="$carta[6]";
+						$contador=$contador+1;
+						$cartaNueva= array("nombre"=>$nombre,"gafas"=>$gafas,"cabello"=>$cabello,"sexo"=>$sexo);
+						$cartasFinal[]=$cartaNueva;	
 		    	}
 				return $cartasFinal;
 			}
@@ -257,12 +224,11 @@
 			}
 			
 
-			$arrayCartaAdivinar=arrayCartas();
+			$arrayCartaAdivinar=arrayCartas();			
 			cartaElegida($arrayCartaAdivinar);
 			$arrayTablero=arrayCartas();
 			echo tableroCartas($arrayTablero);
 		}
-
 	?>
 
 
