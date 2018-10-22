9c6edb714f5ac73ae54bbd04cb927e6cb796c540
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-10-22 20:28:21 +0200

    resetea las preguntas, pero si hay más de una no las limpia todas, acabar el bucle para ello

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var cards;
 var botonHacerPregunta;
 var contadorVolteo = 0;
+var respuestasPosiblesCBox;
+// [[[[[[ELIMINAR ATRIBUTO SI NO SE VA A UTILIZAR (FUNCION PARA EL BOTON)]]]]]
 
 /* pruebas girar carta
 
@@ -48,15 +50,17 @@ function botonActivado() {
 
 function preguntarAlServer() {
 
-    var respuestasPosiblesCBox = [].slice.call(document.getElementsByClassName("cbox"));
+    respuestasPosiblesCBox = [].slice.call(document.getElementsByClassName("cbox"));
 
-    // Si no han respondido = 3, si han respondido una = 2, si han respondido mas de una = 0 o 1
+    // Si no han respondido = 3, si han respondido una = 2
+    // Si han respondido mas de una = 0 o 1
     var semaforo = 0;
     var atributo;
     var id;
     for (var i = 0; i < respuestasPosiblesCBox.length; i++) {
-        if (respuestasPosiblesCBox[i].value != "---" && semaforo <= 1) {
-            //atributo = respuestasPosiblesCBox[i].value; <--- no lo uso pero está bien saber que existe
+        if (respuestasPosiblesCBox[i].value != "---") {
+            // atributo = respuestasPosiblesCBox[i].value;
+            // no lo uso pero está bien saber que existe
             id = respuestasPosiblesCBox[i].getAttribute("class")
             id = id.replace("cbox ","");
         } else {
@@ -65,13 +69,16 @@ function preguntarAlServer() {
     }
 
     if (semaforo == 3) {
-        document.getElementById('texto_salida').innerHTML = "No hay nada seleccionado";
+        document.getElementById('texto_salida').innerHTML =
+        "No hay nada seleccionado";
     
     } else if (semaforo == 2) {
         // Esto es correcto
-        alert(id); // dice el ID de la pregunta (ESTO SIRVE PARA SABER CON QUÉ COMPARAR CON EL SERVER)
+        alert(id); // dice el ID de la pregunta
+        // ESTO SIRVE PARA SABER CON QUÉ COMPARAR CON EL SERVER
     } else if (semaforo == 1 || semaforo == 0) {
-        document.getElementById('texto_salida').innerHTML = "No se pueden seleccionar más de dos elementos";
+        document.getElementById('texto_salida').innerHTML =
+        "No se pueden seleccionar más de dos elementos";
 
     } else {
         document.getElementById('texto_salida').innerHTML = "ERROR";
@@ -83,15 +90,8 @@ function preguntarAlServer() {
 function resetearComboBox(id) {
     var clear = document.getElementById(id);
     clear.selectedIndex = 0;
-    
-    // Hay que editar este bucle para que haga el clear de arriba
-    for (var i = 0; i < respuestasPosiblesCBox.length; i++) {
-        if (respuestasPosiblesCBox[i].value != "---" && semaforo <= 1) {
-            atributo = respuestasPosiblesCBox[i].value;
-            id = respuestasPosiblesCBox[i].getAttribute("class")
-            id = id.replace("cbox ","");
-        }
-    }
+
+    //for (i)
 }
 
 /*
