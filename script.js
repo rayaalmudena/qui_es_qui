21011a80a9c0cf9fd90ca5c27099becc147419bd
rayaalmudena <raya.almudena@gmail.com>
2018-10-26 17:41:24 +0200

    nada

--- script.js
+++ script.js
@@ -446,3 +446,10 @@ function setIntentos(){
     document.modulonombre.intentos.value = contadorPreguntas;
     document.forms["modulonombre"].submit();
 }
+
+function disableF5(e) { if ((e.which || e.keyCode) == 116) e.preventDefault(); };
+// To disable f5
+    /* jQuery < 1.7 */
+$(document).bind("keydown", disableF5);
+/* OR jQuery >= 1.7 */
+$(document).on("keydown", disableF5);
